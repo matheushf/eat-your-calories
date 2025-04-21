a23f62b4f65c69a1f5cba667753ad5d93c45e56d
Matheus Victor <hffmatheus@gmail.com>
2025-04-21 20:24:58 -0300

    min

--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -1,27 +1,7 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
 import TrackerClient from './TrackerClient'
+import { getFoodItems } from './actions'
 
 export default async function TrackerPage() {
-  const cookieStore = cookies()
-  
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        async get(name: string) {
-          return (await cookieStore).get(name)?.value
-        },
-      }
-    }
-  )
-
-  const { data: { session } } = await supabase.auth.getSession()
-  const { data: foods } = await supabase
-    .from('food_items')
-    .select('*')
-    .order('created_at', { ascending: true })
-
-  return <TrackerClient initialFoods={foods || []} userEmail={session?.user.email} />
+  const { foods, userEmail } = await getFoodItems()
+  return <TrackerClient initialFoods={foods} userEmail={userEmail} />
 } 
